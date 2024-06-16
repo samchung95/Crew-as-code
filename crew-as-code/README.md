dde35b6b2a08ddada04ad74b60095a9fa5660fff
Samuel Chung <41113285+samchung95@users.noreply.github.com>
2024-06-17 00:38:05 +0800

    update

---
+++ crew-as-code/README.md
@@ -0,0 +1,7 @@
+# Crew-as-code
+This repository helps to convert yaml configs to a crewai crew. It is a tool to manage your crew as code.
+
+# Todo
+- [ ] Add more examples
+- [ ] Add more documentation
+- [ ] Add tool support
--- crew-as-code/setup.py
+++ crew-as-code/setup.py
@@ -5,15 +5,20 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
+# Read the contents of the requirements.txt file
+requirements_path = this_directory / "requirements.txt"
+with open(requirements_path, 'r') as f:
+    install_requires = f.read().splitlines()
+
 setup(
-    name='your_project_name',
+    name='crew-as-code',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A brief description of your project',
+    author='Samuel Chung',
+    author_email='samuelchung95@gmail.com',
+    description='A simple package to manage your crew as code.',
     long_description=long_description,
     long_description_content_type='text/markdown',  # This is important to correctly render your README.md
-    url='https://github.com/yourusername/yourproject',  # URL to the project's repository
+    url='https://github.com/samchung95/Crew-as-code',  # URL to the project's repository
     packages=find_packages(exclude=['tests', 'tests.*']),
     classifiers=[
         'Programming Language :: Python :: 3',
@@ -21,25 +26,21 @@ setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
-    install_requires=[
-        'required_package1',
-        'required_package2',
-        # Add other dependencies here
-    ],
-    extras_require={
-        'dev': [
-            'check-manifest',
-            'coverage',
-            # Add other development dependencies here
-        ],
-    },
-    entry_points={
-        'console_scripts': [
-            'your_command=your_module:main_function',
-        ],
-    },
+    install_requires=install_requires,
+    # extras_require={
+    #     'dev': [
+    #         'check-manifest',
+    #         'coverage',
+    #         # Add other development dependencies here
+    #     ],
+    # },
+    # entry_points={
+    #     'console_scripts': [
+    #         'your_command=your_module:main_function',
+    #     ],
+    # },
     include_package_data=True,
-    package_data={
-        'your_package': ['data/*.dat'],
-    },
+    # package_data={
+    #     'your_package': ['data/*.dat'],
+    # },
 )
