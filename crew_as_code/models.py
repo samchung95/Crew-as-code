1f6b97440adc2d33c7b1d64b94477c515ee33e27
Samuel Chung <41113285+samchung95@users.noreply.github.com>
2024-07-19 18:06:08 +0800

    Update models.py

--- crew_as_code/models.py
+++ crew_as_code/models.py
@@ -12,6 +12,7 @@ MIXTRAL = "mixtral-8x7b-32768"
 GEMMA = "gemma-7b-it"
 GPT4O = "gpt-4o"
 GPT3TURBO = "gpt-3.5-turbo-0125"
+GPT4OMINI= "gpt-4o-mini"
 
 class LLM:
 
@@ -21,11 +22,16 @@ class LLM:
         self.groq_70b = lambda: ChatGroq(groq_api_key=self.get_groq_api_key(), model_name=GROQ_70B, temperature=1)
         self.mixtral = lambda: ChatGroq(groq_api_key=self.get_groq_api_key(), model_name=MIXTRAL,temperature=0.3)
         self.gemma = lambda: ChatGroq(groq_api_key=self.get_groq_api_key(), model_name=GEMMA,temperature=0.3)
-        self.gpt3turbo = lambda: ChatOpenAI(model=GPT3TURBO,temperature=0.3)
         self.claude35 = lambda: ChatAnthropic(model=CLAUDE_SONNET_35,temperature=0.3)
 
-    def gpt4o(self, temperature:float=0):
+    def gpt4o(self, temperature:float=0.3):
         return ChatOpenAI(model=GPT4O,temperature=temperature)
+    
+    def gpt4omini(self, temperature:float=0.3):
+        return ChatOpenAI(model=GPT4OMINI,temperature=temperature)
+    
+    def gpt3turbo(self, temperature:float=0.3):
+        return ChatOpenAI(model=GPT3TURBO,temperature=temperature)
 
     def get_groq_api_key(self):
         key = self.groq_api_keys[0]
